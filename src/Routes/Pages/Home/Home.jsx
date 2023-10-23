5813aecdf5e4309ce339e2de06908a8bfc46538c
gfaruk02 <gfaruk02@gmail.com>
2023-10-24 00:03:30 +0600

    create banner about

--- src/Routes/Pages/Home/Home.jsx
+++ src/Routes/Pages/Home/Home.jsx
@@ -1,9 +1,12 @@
+import Banner from "../Banner/Banner";
+import About from "./About/About";
 
 
 const Home = () => {
     return (
         <div>
-            <h2>This is Home</h2>
+            <Banner></Banner>
+            <About></About>
         </div>
     );
 };
